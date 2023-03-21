3a6c84a06cab223fb9876bb2d0dbc5358f510ced
beef-erikson <beef.erikson.studios@gmail.com>
2023-03-21 03:10:29 -0400

    productHost directive added through the CLI.

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,13 +4,15 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductComponent } from "./product/product.component";
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { SortPipe } from './sort.pipe';
+import { ProductHostDirective } from './product-host.directive';
 
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    SortPipe
+    SortPipe,
+    ProductHostDirective
   ],
   imports: [
     CommonModule,
