20351e95162813f3d7816e788b0afaa95f11f7c2
beef-erikson <beef.erikson.studios@gmail.com>
2023-03-21 03:23:55 -0400

    Added Offers section; addon via ViewContainerRef to create component dynamically.

--- src/app/products/product-host.directive.ts
+++ src/app/products/product-host.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, OnInit, ViewContainerRef } from '@angular/core';
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 
 @Directive({
   selector: '[appProductHost]'
 })
-export class ProductHostDirective {
 
-  constructor() { }
+export class ProductHostDirective implements OnInit {
+
+  constructor(private vc: ViewContainerRef) { }
+
+  ngOnInit(): void {
+    const productRef = this.vc.createComponent(ProductDetailComponent);
+    productRef.setInput('product', {
+      name: 'Optical mouse',
+      price: 130
+    });
+  }
 
 }
