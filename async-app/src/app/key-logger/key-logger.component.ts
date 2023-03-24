6dc00927e09322cb9574e048106cd5175fe9e5e0
beef-erikson <beef.erikson.studios@gmail.com>
2023-03-24 03:59:00 -0400

    Keylogger added and default HTML wiped/replced with updating title and key logger.

--- async-app/src/app/key-logger/key-logger.component.ts
+++ async-app/src/app/key-logger/key-logger.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent} from "rxjs";
 
 @Component({
   selector: 'app-key-logger',
   templateUrl: './key-logger.component.html',
   styleUrls: ['./key-logger.component.css']
 })
-export class KeyLoggerComponent {
 
+export class KeyLoggerComponent implements OnInit {
+  @ViewChild('keyContainer', { static: true }) input: ElementRef | undefined;
+  keys = '';
+
+  ngOnInit(): void {
+    const logger$ = fromEvent<KeyboardEvent>(this.input?.nativeElement, 'keyup');
+    logger$.subscribe(evt => this.keys += evt.key);
+  }
 }
