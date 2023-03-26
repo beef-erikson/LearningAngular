d5f944d01aff095fc9854243c256daeef31a0f4f
beef-erikson <beef.erikson.studios@gmail.com>
2023-03-26 17:34:46 -0400

    Used takeUntil, Subject to only subscribe until destruction.

--- async-app/src/app/products/product-view/product-view.component.ts
+++ async-app/src/app/products/product-view/product-view.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ProductViewService } from "./product-view.service";
+import { Subject, takeUntil } from "rxjs";
 
 @Component({
   selector: 'app-product-view',
@@ -8,13 +9,15 @@ import { ProductViewService } from "./product-view.service";
   providers: [ProductViewService]
 })
 
-export class ProductViewComponent implements OnInit {
+export class ProductViewComponent implements OnDestroy, OnInit {
   @Input() id = -1;
   name = '';
+  private productSub = new Subject<void>();
+
   constructor(private productViewService: ProductViewService) {}
 
   private getProduct() {
-    this.productViewService.getProduct(this.id).subscribe(product => {
+    this.productViewService.getProduct(this.id).pipe(takeUntil(this.productSub)).subscribe(product => {
       if (product) {
         this.name = product.name;
       }
@@ -22,5 +25,10 @@ export class ProductViewComponent implements OnInit {
   }
   ngOnInit(): void {
     this.getProduct();
+  }
+
+  ngOnDestroy(): void {
+    this.productSub.next();
+    this.productSub.complete();
   }
 }
